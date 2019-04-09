8f8d9966587ee82a0b5e0e14db408a757c582487
Marcin <mwro91@gmail.com>
2019-04-09 19:58:23 +0200

    Initial project add

---
+++ src/main/java/guru/springframework/mysfgjokesapp/MySfgJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mysfgjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySfgJokesAppApplication.class, args);
+    }
+
+}
